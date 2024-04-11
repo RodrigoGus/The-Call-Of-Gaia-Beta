13c2b52d1753e391e1c9bd125f1ab2cbd2630e3e
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-04-11 14:39:49 -0300

    wip: fix double coin collect

--- scripts/coin.cs
+++ scripts/coin.cs
@@ -3,18 +3,24 @@ using System;
 
 public partial class coin : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
+
 	public int coins = 1;
+	public CollisionShape2D _collision;
 	public override void _Ready()
 	{
+		_collision = GetNode<CollisionShape2D>("collision");
+
+
 	}
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public override void _Process(double delta)
 	{
 	}
 	private void OnBodyEntered(Node2D body)
 	{
+		_collision.SetDeferred("disabled", true);
 		GetNode<AnimatedSprite2D>("anim").Play("collected");
+
 		Globals.coins += coins;
 	}
 	private void OnAnimAnimationFinished()
