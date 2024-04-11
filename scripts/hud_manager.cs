8135ecff09331c19b6a3a68d4f7890a0615aef75
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-04-11 14:24:15 -0300

    feat: hud update when player get coins

--- scripts/hud_manager.cs
+++ scripts/hud_manager.cs
@@ -4,20 +4,20 @@ using System;
 public partial class hud_manager : Control
 {
 	public Label _coins_counter;
-	public Label _score_counter;
-	public Label _life_counter;
-	public Label _timer_counter;
+	// public Label _score_counter;
+	// public Label _life_counter;
+	// public Label _timer_counter;
 
 
 	public override void _Ready()
 	{
-		_coins_counter = GetNode<Label>("coins_counter");
-		_score_counter = GetNode<Label>("score_counter");
-		_life_counter = GetNode<Label>("life_counter");
-		_timer_counter = GetNode<Label>("timer_counter");
+		_coins_counter = GetNode<Label>("/root/world1/HUD/control/container/coins_container/coins_counter");
+		// _score_counter = GetNode<Label>("score_counter");
+		// _life_counter = GetNode<Label>("life_counter");
+		// _timer_counter = GetNode<Label>("timer_counter");
 
 		_coins_counter.Text = Globals.coins.ToString();
-		_score_counter.Text = Globals.score.ToString();
+		// _score_counter.Text = Globals.score.ToString();
 	}
 
 	public override void _Process(double delta)
