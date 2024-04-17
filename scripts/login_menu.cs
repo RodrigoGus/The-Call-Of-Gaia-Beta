fbeeae02937c638f3c876bffadb5e431f005df4f
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-04-17 17:12:49 -0300

    wip: database connection

--- scripts/login_menu.cs
+++ scripts/login_menu.cs
@@ -1,98 +1,81 @@
-using Godot;
-// using Npgsql;
-using System;
-using MySql.Data.MySqlClient;
-using System.Data;
-public partial class login_menu : Control
-{
+// using Godot;
+// using MySql.Data.MySqlClient;
+// using System;
+// using System.Data;
 
-	MySql.Data.MySqlClient.MySqlConnection connection;
-	string connectionString = "Server=tcog_db.mysql.dbaas.com.br;Uid=tcog_db;Pwd=tcogdb@T1;Database=tcog_db";
+// public partial class LoginMenu : Control
+// {
+//     private MySqlConnection connection;
+//     private string connectionString = "Server=tcog_db.mysql.dbaas.com.br;Database=tcog_db;Uid=tcog_db;Pwd=tcogdb@T1";
+//     private string sql = "INSERT INTO users (nickname, email, password) VALUES (@nickname, @email, @password)";
 
+//     private TextEdit _nicknameTextbox;
+//     private TextEdit _emailTextbox;
+//     private TextEdit _passwordTextbox;
 
+//     public override void _Ready()
+//     {
+//         _nicknameTextbox = GetNode<TextEdit>("NicknameTextbox");
+//         _emailTextbox = GetNode<TextEdit>("EmailTextbox");
+//         _passwordTextbox = GetNode<TextEdit>("PasswordTextbox");        
 
-	string user_nickname = "";
-	string user_email = "";
-	string user_password = "";
-	TextEdit _nickname_textbox;
-	TextEdit _email_textbox;
-	TextEdit _password_textbox;
+//         TryOpenConnection();
+//     }
 
-	public override void _Ready()
-	{
+//     private void TryOpenConnection()
+//     {
+//         try
+//         {
+//             connection = new MySqlConnection(connectionString);
+//             connection.Open();
+//             GD.Print("Connected to MySQL database.");
+//         }
+//         catch (Exception ex)
+//         {
+//             GD.Print("Error: " + ex.Message);
+//             if (ex.InnerException != null)
+//             {
+//                 GD.Print("Inner Exception: " + ex.InnerException.Message);
+//             }
+//         }
+//     }
 
-		_nickname_textbox = GetNode<TextEdit>("nickname_textbox");
-		_email_textbox = GetNode<TextEdit>("email_textbox");
-		_password_textbox = GetNode<TextEdit>("password_textbox");		
+//     private void _on_SignUpButton_button_down()
+//     {
+        
+//         InsertUser(_nicknameTextbox.Text, _emailTextbox.Text, _passwordTextbox.Text);
+//     }
 
-		try
-		{
-			connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
-			connection.Open();
-			GD.Print("Connected to MySQL database.");
-		}
-		catch (Exception ex)
-		{
-			GD.Print("teste Error: " + ex.Message);
-			if (ex.InnerException != null)
-			{
-				GD.Print("Inner teste Exception: " + ex.InnerException.Message);
-			}
-		}
+//     private void InsertUser(string nickname, string email, string password)
+//     {
+//         if (connection.State != ConnectionState.Open)
+//         {
+//             TryOpenConnection();
+//         }
 
-	}
-	public override void _Process(double delta)
-	{
+//         using (var cmd = new MySqlCommand(sql, connection))
+//         {
+//             cmd.Parameters.AddWithValue("@nickname", nickname);
+//             cmd.Parameters.AddWithValue("@email", email);
+//             cmd.Parameters.AddWithValue("@password", password);
+//             try
+//             {
+//                 cmd.ExecuteNonQuery();
+//                 GD.Print("User inserted successfully.");
+//             }
+//             catch (Exception ex)
+//             {
+//                 GD.Print("Error inserting user: " + ex.Message);
+//             }
+//         }
+//     }
 
-	}
-	private void _on_sign_up_button_button_down()
-	{
-
-		user_nickname = _nickname_textbox.Text;
-		user_email = _email_textbox.Text;
-		user_password = _password_textbox.Text.Sha256Text();
-		InsertUser();
-		
-	}
-	private void InsertUser()
-	{
-		if (connection.State != ConnectionState.Open)
-		{
-			try
-			{
-				connection.Open();
-			}
-			catch (Exception ex)
-			{
-				GD.Print("Error opening connection: " + ex.Message);
-				return;
-			}
-		}
-
-		try
-		{
-			string sql = "INSERT INTO users (nickname, email, password) VALUES (@nickname, @email, @password)";
-			MySqlCommand cmd = new MySqlCommand(sql, connection);
-			cmd.Parameters.AddWithValue("@nickname", user_nickname);
-			cmd.Parameters.AddWithValue("@email", user_email);
-			cmd.Parameters.AddWithValue("@password", user_password);
-			cmd.ExecuteNonQuery();
-			GD.Print("User inserted successfully.");
-		}
-		catch (Exception ex)
-		{
-			GD.Print("Error inserting user: " + ex.Message);
-		}
-		
-	}
-
-	public override void _ExitTree()
-	{
-		if (connection != null && connection.State == ConnectionState.Open)
-		{
-			connection.Close();
-			GD.Print("Connection closed.");
-		}
-	}
-
-}
+//     public override void _ExitTree()
+//     {
+//         if (connection != null && connection.State == ConnectionState.Open)
+//         {
+//             connection.Close();
+//             GD.Print("Connection closed.");
+//         }
+//     }
+// }
