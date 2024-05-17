62ca8cc077c9d7719e74725c3dcfc1192913d348
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-05-17 20:27:36 -0300

    wip: TextBox

--- scripts/TextBox.cs
+++ scripts/TextBox.cs
@@ -5,8 +5,9 @@ public partial class TextBox : MarginContainer
 {
 	public Label label;
 	public Timer timer;
-	public const int MAXWIDTH = 256;
-	public string text = "";
+	public const int MAXWIDTH = 100;
+	[Export]
+	public string text;
 	public int letterIndex = 0;
 	public float letterTime = 0.03f;
 	public float spaceTime = 0.06f;
@@ -16,20 +17,40 @@ public partial class TextBox : MarginContainer
 	{
 		label = GetNode<Label>("MarginContainer/Label");
 		timer = GetNode<Timer>("LetterDisplayTimer");
-		DisplayText("oi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bemoi tudo bem");
+		DisplayText(text);
 	}
 	public override void _Process(double delta)
 	{
-
 	}
 	public void DisplayText(string TextToDisplay)
 	{
-		this.text = TextToDisplay;
-		this.label.Text = "";
 		this.letterIndex = 0;
 
-		if (text.Length > 0)
+		if (TextToDisplay.Length > 0)
 		{
+			// Verificar se o texto é maior que MAXWIDTH
+			if (TextToDisplay.Length > MAXWIDTH)
+			{
+				int lastIndex = 0;
+				for (int i = 0; i < TextToDisplay.Length; i++)
+				{
+					if (i > 0 && i % MAXWIDTH == 0)
+					{
+						label.Text += '\n'; // Adicionar quebra de linha
+						lastIndex = i;
+					}
+					label.Text += TextToDisplay[i];
+				}
+				// Atualizar o índice da próxima letra
+				this.letterIndex = lastIndex + 1;
+			}
+			else
+			{
+				// Texto cabe em uma linha, então não há necessidade de quebra
+				label.Text = TextToDisplay;
+				this.letterIndex = TextToDisplay.Length;
+			}
+
 			ScheduleNextLetter();
 		}
 	}
@@ -53,14 +74,27 @@ public partial class TextBox : MarginContainer
 			timer.Start(delay);
 		}
 	}
-
 	private void OnLetterDisplayTimerTimeout()
-	{
-		if (letterIndex < text.Length)
+{
+	if (letterIndex < text.Length)
 		{
 			label.Text += text[letterIndex];
 			letterIndex++;
 			ScheduleNextLetter();
 		}
+}
+
+	private void OnInteractTextBoxAreaBodyEntered(Node2D body)
+	{
+		if(Input.IsActionJustPressed("E"))
+		{
+			DisplayText(text);
+		}
 	}
 }
+
+
+
+
+
+
